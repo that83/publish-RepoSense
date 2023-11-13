f002a7e71fa6c30ae4a6de46275c9d0a97f9fa6a
that83 <that83@yahoo.com>
2023-11-13 18:26:52 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -15,4 +15,5 @@
 ## Examples of other valid options; For more, please view the user guide
 ### java -jar RepoSense.jar --repos https://github.com/reposense/RepoSense.git
 
-java -jar RepoSense.jar --config ./configs
+java -jar RepoSense.jar --repos https://github.com/connext/xERC20.git
+
